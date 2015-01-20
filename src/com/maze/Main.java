df39daca1716a240213334c209363d32abff3006
Andrii Alieksieienko <andrii.alieksieienko@aeriagames.com>
2015-01-20 23:45:06 +0100

    - started work on maze movement logic

--- src/com/maze/Main.java
+++ src/com/maze/Main.java
@@ -1,12 +1,31 @@
 package com.maze;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
+        char command = '\0';
         GodSingleton God = GodSingleton.getInstance();
         Maze maze = God.createMaze();
         Player player = God.createPlayer();
         player.enterMaze(maze);
-        player.move('N');
-        System.out.print(maze.getRoomsCount());
+        do {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("What we do: ");
+            try{
+                command = br.readLine().charAt(0);
+                if(command == 'P') {
+                    //@TODO: print current maze state here!
+                }
+                else {
+                    player.move(command);
+                }
+            }catch(IOException ex){
+                System.err.println("Word halt!");
+            }
+        }
+        while (command != 'Q');
     }
 }
